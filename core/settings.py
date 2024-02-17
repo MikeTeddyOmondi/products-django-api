453d9e0e329ab80af33a0070fc00985bfa268965
MikeTeddyOmondi <mike_omondi@outlook.com>
2024-02-17 22:01:09 +0300

    Updates - fixes

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
 
@@ -124,7 +125,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- db/init.sql
+++ db/init.sql
@@ -1,1 +1,3 @@
 CREATE DATABASE ecommerce_store;
+
+GRANT ALL ON ecommerce_store.* TO 'user' @'%';
